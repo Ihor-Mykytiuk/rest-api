1d35072a9369b68b2f31bcafabeaa4c5acdbb185
Ihor Mykytiuk <igor14vn@gmail.com>
2025-03-07 17:24:03 +0200

    update Book schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,16 +1,20 @@
-from marshmallow import Schema, fields, validates, ValidationError, post_load
+from marshmallow import Schema, fields, validates, ValidationError, post_load, validate
 from app.models import Book
+from datetime import datetime
+
+MIN_YEAR = 1000
+MAX_YEAR = datetime.now().year
 
 
 class BookSchema(Schema):
-    title = fields.Str(required=True)
-    author = fields.Str(required=True)
+    title = fields.Str(required=True, validate=validate.Length(min=1, max=100))
+    author = fields.Str(required=True, validate=validate.Length(min=1, max=255))
     published_year = fields.Int(required=True)
 
     @validates('published_year')
     def validate_year(self, value):
-        if value < 1000 or value > 2100:
-            raise ValidationError("Published year must be between 1000 and 2100.")
+        if value < MIN_YEAR or value > MAX_YEAR:
+            raise ValidationError(f"Published year must be between {MIN_YEAR} and {MAX_YEAR}.")
 
     @post_load
     def make_book(self, data, **kwargs):
